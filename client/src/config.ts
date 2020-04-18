b6bfc88a5367f5d3c974b2c597ab15aab2948e00
Matthias Holzer <mh@nerdmania.de>
2020-04-18 12:08:40 +0200

    adjust configurationt o point at 0auth

--- client/src/config.ts
+++ client/src/config.ts
@@ -4,7 +4,7 @@ export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-06m895h6.eu.auth0.com', // Auth0 domain
+  clientId: 'R2o2vK49cTOGv0HzDlg33OpUKf4QyQ55', // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
